e2e5440e7a61d0ed5e9ef20875395ae513a651ec
krugo <krugovenkov@mail.ru>
2022-06-02 20:41:12 +0300

    9.1 Зависимость от System.out [#33568]

--- src/test/java/ru/job4j/pojo/ShopDropTest.java
+++ src/test/java/ru/job4j/pojo/ShopDropTest.java
@@ -1,10 +1,9 @@
 package ru.job4j.pojo;
 
 import org.junit.Test;
-
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
 
 public class ShopDropTest {
     @Test
