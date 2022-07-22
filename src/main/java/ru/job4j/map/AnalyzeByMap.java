f554d825896aa27a5d4af9578f0b5b647dd775e0
krugo <krugovenkov@mail.ru>
2022-07-22 10:58:07 +0300

    4. Аттестация [#504878]

---
+++ src/main/java/ru/job4j/map/AnalyzeByMap.java
@@ -0,0 +1,79 @@
+package ru.job4j.map;
+
+import java.util.*;
+
+public class AnalyzeByMap {
+    public static double averageScore(List<Pupil> pupils) {
+        double summ = 0;
+        int amount = 0;
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                summ += subject.score();
+                amount++;
+            }
+        }
+        return summ / amount;
+    }
+
+    public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
+        List<Label> rsl = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            double summ = 0;
+            for (Subject subject : pupil.subjects()) {
+                summ += subject.score();
+            }
+            rsl.add(new Label(pupil.name(), summ / pupil.subjects().size()));
+        }
+        return rsl;
+    }
+
+    public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
+        Map<String, Integer> temps = new HashMap<>();
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                if (!temps.containsKey(subject.name())) {
+                    temps.put(subject.name(), subject.score());
+                } else {
+                    temps.put(subject.name(), subject.score() + temps.get(subject.name()));
+                }
+            }
+        }
+        List<Label> rsl = new ArrayList<>();
+        for (String key : temps.keySet()) {
+            rsl.add(new Label(key, temps.get(key) / pupils.size()));
+        }
+        return rsl;
+    }
+
+    public static Label bestStudent(List<Pupil> pupils) {
+        List<Label> rsl = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            double summ = 0;
+            for (Subject subject : pupil.subjects()) {
+                summ += subject.score();
+            }
+            rsl.add(new Label(pupil.name(), summ));
+        }
+        Collections.sort(rsl);
+        return rsl.get(rsl.size() - 1);
+    }
+
+    public static Label bestSubject(List<Pupil> pupils) {
+        Map<String, Integer> temps = new HashMap<>();
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                if (!temps.containsKey(subject.name())) {
+                    temps.put(subject.name(), subject.score());
+                } else {
+                    temps.put(subject.name(), subject.score() + temps.get(subject.name()));
+                }
+            }
+        }
+        List<Label> rsl = new ArrayList<>();
+        for (String key : temps.keySet()) {
+            rsl.add(new Label(key, temps.get(key)));
+        }
+        Collections.sort(rsl);
+        return rsl.get(rsl.size() - 1);
+    }
+}
